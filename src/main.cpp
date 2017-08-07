f8646ddc04df3e775e3ae8ce0df18f66cf0ed2cc
Anas Youssef Idiab <candfa2660@gmail.com>
2017-08-07 22:51:22 +0200

    Linked SDL_gpu in CMake

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,21 @@
 #include <SDL2/SDL_gpu.h>
 
 int main(int argc, char **argv) {
-    std::cout << "SDL2 Attempting initialization." << std::endl;
+    GPU_Target* form = GPU_Init(1600, 900, 0);
+    
+    /*std::cout << "SDL2 Attempting initialization." << std::endl;
     if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
        std::cout << "SDL2 failed with error: " << SDL_GetError() << std::endl;
        return -1;
     }
     std::cout << "SDL2 Initialized successfully!" << std::endl;
+    */
     
+
     
-    
-    
-    std::cout << "Quitting SDL2." << std::endl;
-    SDL_Quit();
+    std::cout << "Quitting SDL_gpu." << std::endl;
+    GPU_Quit();
+    //std::cout << "Quitting SDL2." << std::endl;
+    //SDL_Quit();
     return 0;
 }
